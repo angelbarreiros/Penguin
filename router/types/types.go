f753c5520c31286dfc079a350657d26d368550a2
angelbarreiros <angel.barreiros@udc.es>
2025-09-09 18:15:37 +0200

    refactor: restructure Date and TimeStamp types
    - Removed BirthDate and NullBirthDate types
    - Simplified Date and TimeStamp with direct marshaling and unmarshaling methods
    - Enhanced Time struct with marshaling and unmarshaling capabilities

--- router/types/types.go
+++ router/types/types.go
@@ -1,36 +1,90 @@
 package types
 
-import (
-	"fmt"
-	"time"
-)
+import "time"
 
-type BirthDate struct {
-	Year  int `json:"year"`
-	Month int `json:"month"`
-	Day   int `json:"day"`
+type Date struct {
+	Year  int
+	Month int
+	Day   int
 }
 
-type NullBirthDate struct {
-	Valid     bool      `json:"valid"`
-	BirthDate BirthDate `json:"birth_date"`
+func (d Date) ToString() string {
+	t := time.Date(d.Year, time.Month(d.Month), d.Day, 0, 0, 0, 0, time.UTC)
+	return t.Format("2006-01-02")
+}
+func (d Date) Marshal() ([]byte, error) {
+	return []byte(d.ToString()), nil
 }
 
-func (b *BirthDate) Marshal() ([]byte, error) {
-	return []byte(b.String()), nil
-
-}
-func (b *BirthDate) Unmarshal(data []byte) error {
+func (d *Date) Unmarshal(data []byte) error {
 	t, err := time.Parse("2006-01-02", string(data))
 	if err != nil {
 		return err
 	}
-	b.Year = t.Year()
-	b.Month = int(t.Month())
-	b.Day = t.Day()
+	d.Year = t.Year()
+	d.Month = int(t.Month())
+	d.Day = t.Day()
 	return nil
 }
 
-func (b *BirthDate) String() string {
-	return fmt.Sprintf("%04d-%02d-%02d", b.Year, b.Month, b.Day)
+type TimeStamp struct {
+	Year        int
+	Month       int
+	Day         int
+	Hour        int
+	Minute      int
+	Second      int
+	Microsecond int
+	Timezone    string
+}
+
+func (t TimeStamp) ToString() string {
+	tm := time.Date(t.Year, time.Month(t.Month), t.Day, t.Hour, t.Minute, t.Second, t.Microsecond*1000, time.UTC)
+	return tm.Format("2006-01-02 15:04:05.000000-07")
+}
+func (t TimeStamp) Marshal() ([]byte, error) {
+	return []byte(t.ToString()), nil
+}
+
+func (t *TimeStamp) Unmarshal(data []byte) error {
+	tm, err := time.Parse("2006-01-02 15:04:05.000000-07", string(data))
+	if err != nil {
+		return err
+	}
+	t.Year = tm.Year()
+	t.Month = int(tm.Month())
+	t.Day = tm.Day()
+	t.Hour = tm.Hour()
+	t.Minute = tm.Minute()
+	t.Second = tm.Second()
+	t.Microsecond = tm.Nanosecond() / 1000
+	t.Timezone = tm.Format("-07")
+	return nil
+}
+
+type Time struct {
+	Hour       int
+	Minute     int
+	Second     int
+	Milisecond int
+}
+
+func (t Time) ToString() string {
+	tm := time.Date(0, 1, 1, t.Hour, t.Minute, t.Second, t.Milisecond*1000000, time.UTC)
+	return tm.Format("15:04:05.000")
+}
+func (t Time) Marshal() ([]byte, error) {
+	return []byte(t.ToString()), nil
+}
+
+func (t *Time) Unmarshal(data []byte) error {
+	tm, err := time.Parse("15:04:05.000", string(data))
+	if err != nil {
+		return err
+	}
+	t.Hour = tm.Hour()
+	t.Minute = tm.Minute()
+	t.Second = tm.Second()
+	t.Milisecond = tm.Nanosecond() / 1000000
+	return nil
 }
